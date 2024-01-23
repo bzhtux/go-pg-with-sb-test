8b487dd0f94dafd2af97395394cf0753505a3d83
Yannick Foeillet <bzhtux@gmail.com>
2024-01-23 13:41:42 +0100

    feat: Add go-gin server listening on port 8080

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"io/fs"
 	"log"
+	"net/http"
 	"path/filepath"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
 	bindings "github.com/bzhtux/servicebinding"
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/copier"
 )
 
@@ -72,24 +74,56 @@ func main() {
 	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		// log.Printf("*** Error connectinng to DB: %s", err.Error())
-		log.Fatal(err.Error())
+		log.Printf(err.Error())
 	}
 
 	fmt.Printf("DBConn: %v", conn)
 	conn.AutoMigrate(&Books{})
 
-	// New DB entry
-	newbook := Books{
-		Title:  "The Hitchhiker's Guide to the Galaxy",
-		Author: "Douglas Adams",
-	}
+	gin.SetMode(gin.DebugMode)
+	router := gin.Default()
 
-	// Ensure entry doesn't exist yet
-	entry := conn.Where("Title = ?", newbook.Title)
-	if entry.RowsAffected != 0 {
-		log.Printf("Book %s already exists in DB", newbook.Title)
-	} else {
-		conn.Create(&newbook)
-		log.Printf("New book %s by %s has been recorded in DB", newbook.Title, newbook.Author)
-	}
+	router.POST("/add", func(c *gin.Context) {
+		// New DB entry
+		newbook := Books{
+			Title:  "The Hitchhiker's Guide to the Galaxy",
+			Author: "Douglas Adams",
+		}
+
+		// Ensure entry doesn't exist yet
+		entry := conn.Where("Title = ?", newbook.Title)
+		if entry.RowsAffected != 0 {
+			log.Printf("Book %s already exists in DB", newbook.Title)
+			c.JSON(http.StatusConflict, gin.H{
+				"status": "Conflict",
+				"data":   "Entry already exists in DB",
+			})
+		} else {
+			conn.Create(&newbook)
+			log.Printf("New book %s by %s has been recorded in DB", newbook.Title, newbook.Author)
+			c.JSON(http.StatusOK, gin.H{
+				"status": "OK",
+				"data":   "New book recorded",
+			})
+		}
+	})
+
+	router.GET("/list", func(c *gin.Context) {
+		var allbooks []Books
+		entries := conn.Find(&allbooks)
+		if entries.RowsAffected == 0 {
+			log.Printf("No entry were found in DB.")
+			c.JSON(http.StatusNotFound, gin.H{
+				"status": "Not Found",
+				"data":   "No record found in DB",
+			})
+		} else {
+			log.Printf("%d record(s) found in DB", entries.RowsAffected)
+			c.JSON(http.StatusOK, gin.H{
+				"data": entries,
+			})
+		}
+	})
+
+	router.Run(":8080")
 }
